6ed0eb0b3d11ce7ecca05a8b4f24a537f1819b6e
Yiming Wei <weiyiming.seu@gmail.com>
2023-06-28 20:42:20 +0800

    304.NumMatrix-二维前缀和

---
+++ NumMatrix.java
@@ -0,0 +1,25 @@
+class NumMatrix {
+    private int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        if(m==0 || n==0) return;
+        sums = new int[m+1][n+1];
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                sums[i+1][j+1] = sums[i][j+1] + sums[i+1][j] - sums[i][j] + matrix[i][j];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2+1][col2+1] - sums[row1][col2+1] - sums[row2+1][col1] + sums[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */

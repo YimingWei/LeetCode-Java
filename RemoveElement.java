20e2eb1464ce0942bfc809d2b75f0a6e9a38ea13
Yiming Wei <weiyiming.seu@gmail.com>
2023-07-06 17:51:54 +0800

    27.RemoveElement-双指针

--- RemoveElement.java
+++ RemoveElement.java
@@ -4,6 +4,7 @@ class Solution {
         int l = 0;
         int r = n - 1;
         while (l <= r) {
+            // 若找到val，则和后面的数交换，因为后面的数不需要考虑
             if (nums[l] == val) {
                 nums[l] = nums[r];
                 r--;

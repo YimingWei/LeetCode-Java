56dab4032baa22233d446d6863c26b139bc5b778
Yiming Wei <weiyiming.seu@gmail.com>
2023-07-06 17:31:40 +0800

    88.Merge-双指针

--- Merge.java
+++ Merge.java
@@ -3,6 +3,7 @@ class Solution {
         int l = m - 1;
         int r = n - 1;
         int s = m + n - 1;
+        // 从后往前遍历
         while (l >= 0 && r >= 0) {
             if (nums1[l] >= nums2[r]) {
                 nums1[s] = nums1[l];
@@ -13,6 +14,7 @@ class Solution {
             }
             s--;
         }
+        // 如果nums1已遍历完而nums2还有数，则将剩余数复制到nums1；若nums1还有数则不需要动
         if (l < 0) {
             while (r >= 0) {
                 nums1[s] = nums2[r];
